b31b5b5a9d17ed85bd60a233348a5641b3f1523b
Rudi Visagie <rudivisagie@icloud.com>
2022-10-25 14:22:20 +0200

    styled cards and changed js to work with all of them. Janky JS fix with proper functions later.

--- website/static/app.js
+++ website/static/app.js
@@ -1,27 +1,78 @@
-const card3d = document.querySelector(".card-3d");
-const container3d = document.querySelector(".container-3d");
-const values = document.querySelector(".values img");
-const title3d = document.querySelector(".title-3d");
-const description3d = document.querySelector(".info-3d h3");
+const card3d = document.querySelectorAll(".card-3d");
+const container3d = document.querySelectorAll(".container-3d");
+const valuesImg = document.querySelectorAll(".values img");
+const title3d = document.querySelectorAll(".title-3d");
+const description3d = document.querySelectorAll(".info-3d h3");
+
+//ToDo: Need to add some loop logic to reduce and reuse code but I just want to get the effect working for now. Maybe 
+//make functions that receive and index and act on it etc. Don't judge the nonsense going on here for now. Again, I know this is horrible. Just look away until
+//I find time to do it properly.
 
 //Moving Animation Event
-container3d.addEventListener("mousemove", (e) => {
+container3d[0].addEventListener("mousemove", (e) => {
     let xAxis = (window.innerWidth / 2 - e.pageX) / 40;
     let yAxis = (window.innerHeight / 2 - e.pageY) / 40;
-    card3d.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    card3d[0].style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;    
+  });
+
+  container3d[1].addEventListener("mousemove", (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 40;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 40;
+    card3d[1].style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+  });
+
+  container3d[2].addEventListener("mousemove", (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 40;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 40;
+    card3d[2].style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
   });
 
 //Animate on Mouse In
-  container3d.addEventListener("mouseenter", (e) => {
-    card3d.style.transition = 'none';
-    // Popout Effect
-    title3d.style.transform = "translateZ(150px)";
+  container3d[0].addEventListener("mouseenter", (e) => {
+      card3d[0].style.transition = 'none';
+      valuesImg[0].style.transform = "translateZ(200px) rotateZ(-30deg)";
+      title3d[0].style.transform = "translateZ(150px)";
+      description3d[0].style.transform = "translateZ(125px)";
+  });
+
+  container3d[1].addEventListener("mouseenter", (e) => {
+      card3d[1].style.transition = 'none';
+      valuesImg[1].style.transform = "translateZ(200px) rotateZ(-30deg)";
+      title3d[1].style.transform = "translateZ(150px)";
+      description3d[1].style.transform = "translateZ(125px)";
+  });
+
+  container3d[2].addEventListener("mouseenter", (e) => {
+      card3d[2].style.transition = 'none';
+      valuesImg[2].style.transform = "translateZ(200px) rotateZ(-30deg)";
+      title3d[2].style.transform = "translateZ(150px)";
+      description3d[2].style.transform = "translateZ(125px)";
   });
 
 //Animate on Mouse Out
-container3d.addEventListener("mouseleave", (e) => {
-    card3d.style.transform = 'rotateY(0deg) rotateX(0deg)';
-    card3d.style.transition = 'all 0.5s ease';
+container3d[0].addEventListener("mouseleave", (e) => {
+    card3d[0].style.transform = 'rotateY(0deg) rotateX(0deg)';
+    card3d[0].style.transition = 'all 0.5s ease';
     //Popout Reverse
-    title3d.style.transform = "translateZ(0px)";
+    title3d[0].style.transform = "translateZ(0px)";
+    valuesImg[0].style.transform = "translateZ(0px)";
+    description3d[0].style.transform = "translateZ(0px)";
+  });
+
+container3d[1].addEventListener("mouseleave", (e) => {
+    card3d[1].style.transform = 'rotateY(0deg) rotateX(0deg)';
+    card3d[1].style.transition = 'all 0.5s ease';
+    //Popout Reverse
+    title3d[1].style.transform = "translateZ(0px)";
+    valuesImg[1].style.transform = "translateZ(0px)";
+    description3d[1].style.transform = "translateZ(0px)";
+  });
+
+container3d[2].addEventListener("mouseleave", (e) => {
+    card3d[2].style.transform = 'rotateY(0deg) rotateX(0deg)';
+    card3d[2].style.transition = 'all 0.5s ease';
+    //Popout Reverse
+    title3d[2].style.transform = "translateZ(0px)";
+    valuesImg[2].style.transform = "translateZ(0px)";
+    description3d[2].style.transform = "translateZ(0px)";
   });
